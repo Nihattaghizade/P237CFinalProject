8d0a42f723576d7ef73ab8f0d4eaaae3566e64c0
hmaleyka <maleykaheybatova1011@gmail.com>
2023-11-02 22:59:55 +0400

    4-cu commit

---
+++ P237C#FinalProject/Academy.Service/Services/Implementations/StudentService.cs
@@ -0,0 +1,36 @@
+﻿using Academy.Core.Enums;
+using Academy.Core.Repositories;
+using Academy.Data.Repositories;
+using Academy.Service.Services.Interfaces;
+
+namespace Academy.Service.Services.Implementations
+{
+    public class StudentService : IStudentService
+    {
+        IStudentRepository _studentRepository=new StudentRepository();
+        public Task<string> CreateAsync(string fullName, string group, double avarage, EducationType educationType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> RemoveAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> UpdateAsync(string id, string fullName, string group, double avarage, EducationType educationType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ P237C#FinalProject/Academy.Service/Services/Interfaces/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using Academy.Core.Enums;
+
+namespace Academy.Service.Services.Interfaces
+{
+    public interface IStudentService
+    {
+        public Task<string> CreateAsync(string fullName,string group,double avarage,EducationType educationType);
+        public Task<string> UpdateAsync(string id,string fullName, string group, double avarage, EducationType educationType);
+        public Task<string> RemoveAsync(string id);
+        public Task<string> GetAsync(string id);
+        public Task GetAllAsync();
+    }
+}
